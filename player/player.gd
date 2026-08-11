e08555a57f635b4cda11066731a6f84b590e8347
Joshua Jenner <joshua-jenner@hotmail.com>
2026-08-12 00:07:26 +0800

    Updated player

--- player/player.gd
+++ player/player.gd
@@ -1,17 +1,21 @@
 extends CharacterBody2D
 
 
-var move_speed: float = 100
+var move_speed: float = 200
+var jump_speed: float = -400
 
 
 func _ready() -> void:
 	pass
 
 
-func get_input():
-	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = input_direction * move_speed
-
-func _physics_process(delta):
-	get_input()
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if Input.is_action_just_pressed("move_up") and is_on_floor():
+		velocity.y = jump_speed
+	
+	var input_direction: float = Input.get_axis("move_left", "move_right")
+	velocity.x = input_direction * move_speed
 	move_and_slide()
