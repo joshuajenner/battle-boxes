4383c92c53dc82c36f0d99380df667e0aa94eb47
Joshua Jenner <joshua-jenner@hotmail.com>
2026-08-27 15:58:22 +0800

    Added credits

--- autoloads/scenes.gd
+++ autoloads/scenes.gd
@@ -6,6 +6,7 @@ const ALL_SCENES: Array[String] = [
 	MAIN_MENU,
 	LEVEL_SELECT,
 	SETTINGS_MENU,
+	CREDITS,
 	GAME_LEVEL,
 ]
 
@@ -13,5 +14,6 @@ const ALL_SCENES: Array[String] = [
 const MAIN_MENU: String = "res://menus/main_menu.tscn"
 const LEVEL_SELECT: String = "res://menus/level_select.tscn"
 const SETTINGS_MENU: String = "res://menus/settings_menu.tscn"
+const CREDITS: String = "res://menus/credits.tscn"
 
 const GAME_LEVEL: String = "res://game/game_level.tscn"
--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -3,11 +3,13 @@ extends Control
 
 @export var play_button: Button
 @export var settings_button: Button
+@export var credits_button: Button
 
 
 func _ready() -> void:
 	play_button.pressed.connect(on_play_button_pressed)
 	settings_button.pressed.connect(on_settings_button_pressed)
+	credits_button.pressed.connect(on_credits_button_pressed)
 	GameData.load_game_data()
 
 
@@ -17,3 +19,7 @@ func on_play_button_pressed() -> void:
 
 func on_settings_button_pressed() -> void:
 	SceneManager.switch_to(Scenes.SETTINGS_MENU)
+
+
+func on_credits_button_pressed() -> void:
+	SceneManager.switch_to(Scenes.CREDITS)
