c0e1aa879f9d7fbbec0e194e228ca59fbceaf3c3
Joshua Jenner <joshua-jenner@hotmail.com>
2026-09-07 22:11:16 +0800

    Update hurt_box_component.gd

--- components/hurt_box_component.gd
+++ components/hurt_box_component.gd
@@ -5,6 +5,10 @@ extends Area2D
 signal projectile_entered(damage: int)
 
 
-func _on_area_entered(area: Area2D) -> void:
+func _ready() -> void:
+	area_entered.connect(on_area_entered)
+
+
+func on_area_entered(area: Area2D) -> void:
 	if area is Projectile:
 		projectile_entered.emit(area.damage)
