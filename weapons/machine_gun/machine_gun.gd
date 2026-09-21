db6ec4ee9f0b3109e1f730b461898d7358ccecaa
Joshua Jenner <joshua-jenner@hotmail.com>
2026-09-21 16:54:11 +0800

    Shortened machine gun

--- weapons/machine_gun/machine_gun.gd
+++ weapons/machine_gun/machine_gun.gd
@@ -10,12 +10,13 @@ extends Weapon
 
 var rng = RandomNumberGenerator.new()
 
+
 func fire() -> void:
 	var bullet: Node2D = bullet_scene.instantiate()
 	bullet.damage = damage
 	bullet.global_position = muzzle.global_position
 	bullet.direction = Vector2(direction_x, 
-			rng.randf_range(-bullet_spread, bullet_spread))
+	rng.randf_range(-bullet_spread, bullet_spread))
 	projetile_parent_node.add_child(bullet)
 	weapon_audio.play_random_pitch()
 	animation_player.stop()
