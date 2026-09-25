ab4af7037dc3bce8771f569b99a2861683cd4eaa
Joshua Jenner <joshua-jenner@hotmail.com>
2026-09-26 01:17:58 +0800

    Buffed mini gun

--- weapons/mini_gun/mini_gun.gd
+++ weapons/mini_gun/mini_gun.gd
@@ -1,6 +1,5 @@
 extends Weapon
 
-
 @export var bullet_scene: PackedScene
 @export var muzzle: Marker2D
 @export var weapon_audio: WeaponAudioPlayer
@@ -20,5 +19,5 @@ func fire() -> void:
 			rng.randf_range(-bullet_spread_up, bullet_spread_down))
 	projetile_parent_node.add_child(bullet)
 	weapon_audio.play_random_pitch()
-	animation_player.stop()
+	#animation_player.stop()
 	animation_player.play("fire")
